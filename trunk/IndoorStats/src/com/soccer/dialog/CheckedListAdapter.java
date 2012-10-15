f7ace4b23ece84b019d4a389770a49b971239f0c
guykirschbaum@gmail.com <guykirschbaum@gmail.com@ddd49791-9daa-de7b-f953-8bab07db401c>
2012-10-15 21:01:38 +0000

    support for recording in game events

--- trunk/IndoorStats/src/com/soccer/dialog/CheckedListAdapter.java
+++ trunk/IndoorStats/src/com/soccer/dialog/CheckedListAdapter.java
@@ -3,66 +3,120 @@ package com.soccer.dialog;
 import java.util.ArrayList;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.soccer.indoorstats.R;
+import com.soccer.indoorstats.ingame.GameEvent;
+import com.soccer.indoorstats.ingame.IGameEvent;
 
 public class CheckedListAdapter extends BaseAdapter {
-    
-    private Activity activity;
-    private ArrayList<lstItem> data = new ArrayList<lstItem>();
-    private static LayoutInflater inflater=null;
-    
-    public CheckedListAdapter(Activity a, ArrayList<lstItem> d) {
-        activity = a;
-        setData(d);
-        inflater = (LayoutInflater)activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-    }
 
-    public int getCount() {
-        return data.size();
-    }
+	private Activity activity;
+	private ArrayList<lstItem> data = new ArrayList<lstItem>();
+	private static LayoutInflater inflater = null;
 
-    public Object getItem(int position) {
-        return data.get(position);
-    }
+	public CheckedListAdapter(Activity a, ArrayList<lstItem> d) {
+		activity = a;
+		setData(d);
+		inflater = (LayoutInflater) activity
+				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+	}
 
-    public long getItemId(int position) {
-        return position;
-    }
-    
-    public void setData(ArrayList<lstItem> arr) {
-    	data.clear();
-    	for(int i=0; i<arr.size(); i++) {
-        	if(arr.get(i).mChecked)
-        		data.add(arr.get(i));
-        }
-    	notifyDataSetChanged();
-    }
-    
-    public View getView(int position, View convertView, ViewGroup parent) {
-        View vi=convertView;
-        if(convertView==null)
-            vi = inflater.inflate(R.layout.player_row_in_game, null);
+	public int getCount() {
+		return data.size();
+	}
 
-        TextView txtFirst = (TextView)vi.findViewById(R.id.pfname);
-        RelativeLayout rlToTag = (RelativeLayout)vi.findViewById(R.id.pplayertaglayout);
-        rlToTag.setTag(data.get(position).mId);
-        if(data.get(position).mChecked) {
-        	txtFirst.setText(data.get(position).mText);
-        }
-        else
-        	vi.setVisibility(View.INVISIBLE);
-        
-        return vi;
-    }
-    
-    
+	public Object getItem(int position) {
+		return data.get(position);
+	}
+
+	public long getItemId(int position) {
+		return position;
+	}
+
+	public void setData(ArrayList<lstItem> arr) {
+		data.clear();
+		for (int i = 0; i < arr.size(); i++) {
+			if (arr.get(i).mChecked)
+				data.add(arr.get(i));
+		}
+		notifyDataSetChanged();
+	}
+
+	public View getView(final int position, View convertView, ViewGroup parent) {
+		View vi = convertView;
+		if (convertView == null)
+			vi = inflater.inflate(R.layout.player_row_in_game, null);
+
+		TextView txtFirst = (TextView) vi.findViewById(R.id.pfname);
+		RelativeLayout rlToTag = (RelativeLayout) vi
+				.findViewById(R.id.pplayertaglayout);
+		rlToTag.setTag(data.get(position).mId);
+		rlToTag.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(final View v) {
+				final CharSequence[] items = { "Goal", "Cook", "Own goal", "Yellow card",
+						"Red card", "-Reset-" };
+
+				AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+				builder.setTitle("Event");
+				builder.setItems(items, new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int item) {
+						Toast.makeText(
+								v.getContext(),
+								data.get(position).mText
+										+ " received " + items[item],
+								Toast.LENGTH_SHORT).show();
+						IGameEvent.EventType evt = IGameEvent.EventType.Goal;;
+						boolean resetEvents = false;
+						switch(item) {
+						case 0:
+							evt = IGameEvent.EventType.Goal;
+							break;
+						case 1:
+							evt = IGameEvent.EventType.Cook;
+							break;
+						case 2:
+							evt = IGameEvent.EventType.O_Goal;
+							break;
+						case 3:
+							evt = IGameEvent.EventType.Y_Card;
+							break;
+						case 4:
+							evt = IGameEvent.EventType.R_Card;
+							break;
+						case 5:
+							resetEvents = true;
+						}
+						if(resetEvents)
+							data.get(position).mEvents.clear();
+						else
+							data.get(position).addEvent(new GameEvent(evt, 0));
+						notifyDataSetChanged();
+					}
+				});
+				AlertDialog alert = builder.create();
+				alert.show();
+			}
+		});
+
+		if (data.get(position).mChecked) {
+			String text = new String(data.get(position).mText);
+			txtFirst.setText(new String(data.get(position).toStringEvents()));
+		} else
+			vi.setVisibility(View.INVISIBLE);
+
+		return vi;
+	}
 }
--- trunk/IndoorStats/src/com/soccer/dialog/lstItem.java
+++ trunk/IndoorStats/src/com/soccer/dialog/lstItem.java
@@ -1,22 +1,61 @@
 package com.soccer.dialog;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
-public class lstItem implements Serializable{
+import com.soccer.indoorstats.ingame.IGameEvent;
+
+public class lstItem implements Serializable {
 	private static final long serialVersionUID = 6983509531169847290L;
 	public String mText;
 	public String mId;
 	public boolean mChecked;
+	public ArrayList<IGameEvent> mEvents = new ArrayList<IGameEvent>();
 
 	public lstItem(String text, String id, boolean checked) {
 		mText = text;
 		mId = id;
 		mChecked = checked;
-
 	}
-	
+
+	public void addEvent(IGameEvent evt) {
+		mEvents.add(evt);
+	}
+
+	public void removeEvent(IGameEvent evt) {
+		mEvents.remove(evt);
+	}
+
 	@Override
-    public String toString() {
-        return this.mText;
-    }
+	public String toString() {
+		return this.mText;
+	}
+
+	public String toStringEvents() {
+		String evts = "";
+		int g = 0, og = 0, r = 0, y = 0, c = 0;
+		for (IGameEvent ge : mEvents) {
+			switch (ge.getEventType()) {
+			case Goal:
+				g++;
+				break;
+			case O_Goal:
+				og++;
+				break;
+			case Y_Card:
+				y++;
+				break;
+			case R_Card:
+				r++;
+				break;
+			case Cook:
+				c++;
+				break;
+			}
+		}
+		evts = this.mText
+				+ (((g + og + r + y + c) > 0) ? "(" + g + "," + og + "," + r
+						+ "," + y + "," + c + ")" : "");
+		return evts;
+	}
 }
